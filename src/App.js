079d697d1808a8c265d920d40b3ce02702fa6ecc
anandgarikapati <garikapati.2014@gmail.com>
2022-01-27 19:12:41 +0530

    Recipe_search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import ListItems from './displayList'
+
 
 function App() {
+  const [search, setSearch] = useState(' ');
+  const [data, setData] = useState([])
+
+  
+  const listNumbers = [1,2,3,4,5,6,7,8]
+  function submitHandler(e) {
+    e.preventDefault()
+    // console.log(search)
+    const YOUR_APP_ID = "a293352b";
+    const YOUR_APP_KEY ="9bc036c37df2a0dea0509bbf5978b661";
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=25`)
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => setData(data.hits))
+
+    
+    
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+       <h1 className='heading'>Foodie Adda</h1>
+      </div>
+        <div className='navbar'>
+          <form onSubmit={submitHandler} className='inputfield' >
+            <input type='text' className='inputtext' value= {search} onChange={(e) => setSearch(e.target.value)} ></input>
+            <input type='submit' className='search' value='Search'></input>
+          </form>
+       </div>
+       <div>
+       <ListItems key={listNumbers} data={data}/>
+       </div>
     </div>
   );
 }
---
+++ src/displayList.js
@@ -0,0 +1,31 @@
+import Recat, {useState} from 'react'
+
+const ListItems = ({data}) => {
+    return(
+        <div className='list'>
+            {data.map(data => 
+                
+                <div className='child1'>
+                    <img src={data.recipe.image}></img>
+
+                    <div >
+                        <h2 className='label'>{data.recipe.label}</h2>
+
+                     <div className='child2'>
+                       <h5>CuisineType: {data.recipe.cuisineType}</h5>
+                       <h5>Calories: {Math.round(data.recipe.calories)}</h5>
+                       <h5>DietLabels:  {data.recipe.dietLabels}</h5>  
+                       <button className='btn'>Order Now</button>  
+                     </div>   
+                        
+                    </div>
+                    
+                </div>
+
+                
+                )}
+        </div>
+    )
+}
+
+export default ListItems
